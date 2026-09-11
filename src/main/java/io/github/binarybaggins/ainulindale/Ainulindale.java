a5d8857d08fb9b82b99698826b8cab78eedefdf7
BinaryBaggins <80835556+BinaryBaggins@users.noreply.github.com>
2026-09-11 10:50:31 +0200

    Initialize standalone Maven project

---
+++ src/main/java/io/github/binarybaggins/ainulindale/Ainulindale.java
@@ -0,0 +1,10 @@
+package io.github.binarybaggins.ainulindale;
+
+public class Ainulindale {
+    private Ainulindale() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Ainulindalë");
+    }
+}
