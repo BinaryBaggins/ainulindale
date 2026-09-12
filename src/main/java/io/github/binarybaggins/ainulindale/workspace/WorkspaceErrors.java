fba76fb32503912d8a8c20b5faeb7f4771e32b9c
SapphireFlame96 <80835556+BinaryBaggins@users.noreply.github.com>
2026-09-12 15:16:05 +0200

    Create WorkspaceErrors.java

---
+++ src/main/java/io/github/binarybaggins/ainulindale/workspace/WorkspaceErrors.java
@@ -0,0 +1,29 @@
+package io.github.binarybaggins.ainulindale.workspace;
+
+import io.github.binarybaggins.ainulindale.core.result.ResultError;
+
+public class WorkspaceErrors {
+    
+    private WorkspaceErrors() {}
+
+    public static final ResultError TRACK_ALREADY_PRESENT = new ResultError(
+        "Workspace.TrackAlreadyPresent"
+    );
+
+    public static final ResultError TRACK_NOT_FOUND = new ResultError(
+        "Workspace.TrackNotFound"
+    );
+
+    public static final ResultError TRACK_NOT_VISIBLE = new ResultError(
+        "Workspace.TrackNotVisible"
+    );
+
+    public static final ResultError ACTIVE_TRACK_CANNOT_BE_HIDDEN = new ResultError(
+        "Workspace.ActiveTrackCannotBeHidden"
+    );
+
+    public static final ResultError ACTIVE_TRACK_CANNOT_BE_REMOVED = new ResultError(
+        "Workspace.ActiveTrackCannotBeRemoved"
+    );
+
+}
